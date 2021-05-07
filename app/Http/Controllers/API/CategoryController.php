c007b66f0265decff06408bac4a45165d7e50469
Roula Rohban <r.rohban@aldrtechnology.com>
2021-05-07 18:31:19 +0300

    Categories Parent & Child APIs

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function getParents(){
+        $categories = Category::whereNull('parent_id')->get(['id','name']);
+        return response()->json(['parents' => $categories ],200);
+    }
+    public function getChild($parent_id){
+        $child = Category::with('subCategories')->where('parent_id',$parent_id)->get();
+        return response()->json(['child' => $child ],200);
+    }
 }
